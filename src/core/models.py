dc965ac8caebc0a9f534ed164b15bf84409008f5
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-07 13:33:13 +0300

    Add database (sqlalchemy and alembic)

---
+++ src/core/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer
+
+from .db import Base
+
+
+class AvitoRequest(Base):
+    __tablename__ = 'avito_requests'
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    text = Column(String(255))
+    region = Column(String(255))
