d3a1801eadafe51b7d28c541f9f6235741f6ae70
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-08 17:23:30 +0300

    Add table for request values

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 
 from core.db import Base
 
@@ -9,3 +9,12 @@ class AvitoRequest(Base):
     id = Column(Integer, primary_key=True, index=True, unique=True)
     text = Column(String(255))
     region = Column(String(255))
+
+
+class RequestValues(Base):
+    __tablename__ = 'request_values'
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    avito_request_id = Column(Integer, ForeignKey("avito_requests.id"))
+    datetime = Column(DateTime)
+    value = Column(Integer)
