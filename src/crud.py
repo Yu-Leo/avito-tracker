90a5f11fcbab98e2765f52c71c40c1465e166de5
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-08 17:58:13 +0300

    Add getting request values

--- src/crud.py
+++ src/crud.py
@@ -12,7 +12,8 @@ def create_avito_request(db: Session, item: AvitoRequestCreate) -> AvitoRequest:
     db.refresh(avito_request)
     return avito_request
 
-def create_request_value(db: Session, item: AvitoRequest):
+
+def create_request_value(db: Session, item: AvitoRequest) -> RequestValues:
     request_value = RequestValues(avito_request_id=item.id,
                                   datetime=datetime.datetime.now(),
                                   value=1)
@@ -20,3 +21,8 @@ def create_request_value(db: Session, item: AvitoRequest):
     db.commit()
     db.refresh(request_value)
     return request_value
+
+
+def get_request_values(db: Session, id: int, start: str, end: str) -> list:
+    request_values = list(db.query(RequestValues).filter(RequestValues.avito_request_id == id))
+    return request_values
