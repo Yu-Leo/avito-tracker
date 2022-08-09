356e2d8916ed444aebdb3c47e68989f8b5e11c56
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-09 09:34:22 +0300

    Add thread with parser

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+import threading
+
+import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import Response
 
+import crud
 from core.db import SessionLocal
 from handlers import router
 
@@ -20,3 +24,8 @@ async def db_session_middleware(request: Request, call_next):
 
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    thr = threading.Thread(target=crud.regular_parse, args=(10,), name='thr-1', daemon=True)
+    thr.start()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
