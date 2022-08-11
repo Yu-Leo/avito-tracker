a29300200d8697266ee30a409a835d5adcc38605
Lev Yuvensky <levayu22@gmail.com>
2022-08-11 10:14:09 +0300

    Add catching invalid datetime

--- src/tracker/api/avito_queries.py
+++ src/tracker/api/avito_queries.py
@@ -1,21 +1,18 @@
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 
+from tracker import utils
 from tracker.schemas import AvitoQueryCreate, AvitoQueryValueBase
-from tracker.services.avito_parser import is_avito_query_correct
 from tracker.services.avito_queries import AvitoQueryService, AvitoQueryValueService
 
 router = APIRouter()
 
 
 @router.post('/add')
-def add_avito_query(item: AvitoQueryCreate, avito_query_service: AvitoQueryService = Depends()):
-    if not is_avito_query_correct(item):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Invalid value in the \'query\' or \'region\' field (for avito.ru)')
-    avito_query = avito_query_service.create(item)
+def add_avito_query(avito_query_data: AvitoQueryCreate, avito_query_service: AvitoQueryService = Depends()):
+    utils.check_avito_query(avito_query_data)
+    avito_query = avito_query_service.create(avito_query_data)
     return {'id': avito_query.id}
 
 
@@ -24,5 +21,6 @@ def get_avito_query_values(avito_query_id: int,
                            start: Optional[str] = None,
                            end: Optional[str] = None,
                            avito_query_value_service: AvitoQueryValueService = Depends()):
+    utils.check_start_and_end_datetime(start, end)
     avito_query_values = avito_query_value_service.get_by_avito_query_id(avito_query_id, start, end)
     return avito_query_values
