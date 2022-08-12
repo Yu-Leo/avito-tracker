e04c1d2c99c23018dcf62c0bfdfef6df0ee63b36
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-12 16:52:58 +0300

    Refactor fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,28 @@ from sqlalchemy.orm import sessionmaker
 from tracker.models import Base
 
 
-@pytest.fixture()
-def database_session():
+@pytest.fixture
+def create_database():
     # Set up
     SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    TestingSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     Base.metadata.create_all(bind=engine)
 
-    session = TestingSessionLocal()
+    return TestingSession
 
-    yield session  # Testing
 
-    # Tear down
-    session.close()
+@pytest.fixture
+def database_session(create_database):
+    TestingSession = create_database
+
+    session = TestingSession()
+
+    try:
+        yield session  # Run test wit session
+    finally:  # Tear down
+        session.close()
