cbf99ed072b44aaabeb7dd6947251a6f4382072a
Lev Yuvenskiy <levayu22@gmail.com>
2022-08-13 10:25:28 +0300

    Add tests for services.avito_queries

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
+import datetime
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from tracker import models
 from tracker.models import Base
+from tracker.schemas import AvitoQueryValueCreate, AvitoQueryCreate
 
 
 @pytest.fixture
@@ -30,3 +34,41 @@ def database_session(create_database):
         yield session  # Run test wit session
     finally:  # Tear down
         session.close()
+
+
+@pytest.fixture
+def avito_queries_list(database_session):
+    avito_queries_list = []
+    for i in range(10):
+        avito_query_data = AvitoQueryCreate(query=f'query{i}', region=f'region{i}')
+        avito_query_object = models.AvitoQuery(**avito_query_data.dict())
+        avito_queries_list.append(avito_query_object)
+        database_session.add(avito_query_object)
+    database_session.commit()
+    return avito_queries_list
+
+
+@pytest.fixture
+def avito_query_values_list(database_session):
+    database_session.add(models.AvitoQueryValue(**AvitoQueryValueCreate(
+        avito_query_id=1,
+        timestamp=datetime.datetime(2022, 8, 13, 0, 0, 0, 0),
+        value=1
+    ).dict()))
+    database_session.add(models.AvitoQueryValue(**AvitoQueryValueCreate(
+        avito_query_id=2,
+        timestamp=datetime.datetime(2022, 8, 13, 0, 0, 0, 0),
+        value=1
+    ).dict()))
+    database_session.add(models.AvitoQueryValue(**AvitoQueryValueCreate(
+        avito_query_id=1,
+        timestamp=datetime.datetime(2020, 8, 13, 0, 0, 0, 0),
+        value=1
+    ).dict()))
+    database_session.add(models.AvitoQueryValue(**AvitoQueryValueCreate(
+        avito_query_id=1,
+        timestamp=datetime.datetime(2023, 8, 13, 0, 0, 0, 0),
+        value=1
+    ).dict()))
+
+    database_session.commit()
