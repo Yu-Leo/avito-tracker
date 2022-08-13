f2e85b89f97584c445ba18aa2e0e6f1f478260f1
Lev Yuvensky <levayu22@gmail.com>
2022-08-13 14:44:44 +0300

    Add test /add

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,15 @@
 import pytest
 
+from tracker import models
+
 
 @pytest.mark.parametrize('data', [{'query': 'book',
                                    'region': 'moskva'}])
-def test_add(data, client):
+def test_add(data, client, session):
+    # Act
     response = client.post('/add', json=data)
+
+    # Assert
     assert response.status_code == 200
+    object_from_db = session.query(models.AvitoQuery).first()
+    assert object_from_db.query == data['query'] and object_from_db.region == data['region']
