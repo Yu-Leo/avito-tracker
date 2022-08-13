c9a03a871e56abd7667b7584daafa6d309d6741b
Lev Yuvensky <levayu22@gmail.com>
2022-08-13 13:44:23 +0300

    Add negative testcases with mocked requests & refactor positive testcases

--- tests/save_pages_content.py
+++ tests/save_pages_content.py
@@ -1,19 +1,31 @@
+from typing import Optional
+
 import requests
 
 
-def get_and_save_page_content(query: str, region: str):
+def get_and_save_page_content(query: str, region: str, filename: Optional[str] = None):
+    if not filename:
+        filename = f'{query}_{region}.txt'
     url = f'https://www.avito.ru/{region}?q={query}'
     request = requests.get(url)
     content = request.content
-    with open(f'pages_content/{query}_{region}.txt', 'wb') as file:
+    with open(f'pages_content/{filename}', 'wb') as file:
         file.write(content)
 
 
-def main(avito_queries: list[tuple[str, str]]):
-    for avito_query in avito_queries:
+def main(correct_avito_queries: Optional[list[tuple[str, str]]] = None,
+         incorrect_avito_queries: Optional[list[tuple[str, str]]] = None) -> None:
+    if correct_avito_queries is None:
+        correct_avito_queries = []
+    if incorrect_avito_queries is None:
+        incorrect_avito_queries = []
+
+    for avito_query in correct_avito_queries:
         get_and_save_page_content(*avito_query)
+    for i in range(len(incorrect_avito_queries)):
+        get_and_save_page_content(*incorrect_avito_queries[i], filename=f'incorrect_query_{i}.txt')
 
 
 if __name__ == '__main__':
-    main([('book', 'moskva'),
-          ('laptop', 'moskva'), ])
+    main([('book', 'moskva'), ('laptop', 'moskva'), ],
+         [('123', '123')])
